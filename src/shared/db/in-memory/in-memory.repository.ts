96e02d98bf696c6587f15717b8aa4db6bb49046f
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-11-28 00:32:36 +0000

    feature(development): custom error for non-existent entity

--- src/shared/db/in-memory/in-memory.repository.ts
+++ src/shared/db/in-memory/in-memory.repository.ts
@@ -1,4 +1,5 @@
 import { Entity } from "../../domain/entity";
+import { NotFoundError } from "../../domain/errors/not-found.error";
 import { IRepository } from "../../domain/repository/repository-interface";
 import { ValueObject } from "../../domain/value-object";
 
@@ -22,7 +23,7 @@ export abstract class InMemoryRepository<
       item.entity_id.equals(entity.entity_id)
     );
     if (indexFound === -1) {
-      throw new Error("Entity not found");
+      throw new NotFoundError(entity.entity_id, this.getEntity());
     }
     this.items[indexFound] = entity;
   }
@@ -32,7 +33,7 @@ export abstract class InMemoryRepository<
       item.entity_id.equals(entity_id)
     );
     if (indexFound === -1) {
-      throw new Error("Entity not found");
+      throw new NotFoundError(entity_id, this.getEntity());
     }
   }
 
